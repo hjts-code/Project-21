3cb08f28937342c35c582a8c67a1a35b1ac2c197
Euzinho21 <85627786+Euzinho21@users.noreply.github.com>
2022-01-19 16:39:15 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var bloco1,bloco2,bloco3,bloco4;
+var bola;
+var arestas;
+
+function setup(){
+    createCanvas(850,600);
+
+    bloco1 = createSprite(0,580,360,30);
+    bloco1.shapeColor = "blue";
+
+    bloco2 = createSprite(295,580,200,30);
+    bloco2.shapeColor = "orange";
+
+    bloco3 = createSprite(515,580,200,30);
+    bloco3.shapeColor = "pink";
+
+    bloco4 = createSprite(735,580,200,30);
+    bloco4.shapeColor = "red";
+
+    bola = createSprite(400, 100, 40,40);
+    bola.shapeColor = rgb(255,255,255);
+    bola.velocityX+=4;
+    bola.velocityY+=4;
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    arestas=createEdgeSprites();
+    bola.bounceOff(arestas);
+
+    
+    //escreva o código de ricochete de bola para bloco1 
+    if(bloco1.isTouching(bola) && bola.bounceOff(bloco1)){
+        bola.shapeColor = "blue";
+    }
+
+
+    if(bloco2.isTouching(bola)){
+        bola.shapeColor = "orange";
+        bola.velocityX = 0;
+        bola.velocityY = 0;
+    }
+
+    if(bloco3.isTouching(bola) && bola.bounceOff(bloco3)){
+        bola.shapeColor = "pink";
+    }
+
+    if(bloco4.isTouching(bola) && bola.bounceOff(bloco4)){
+        bola.shapeColor = "red";
+    }
+
+    drawSprites();
+}
